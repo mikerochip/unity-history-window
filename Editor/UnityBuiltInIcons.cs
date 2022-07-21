a73468ea1cfaf5a8cac371ab7d578448fb1b085b
Mike Schweitzer <mschweitzer@gmail.com>
2022-07-20 22:18:57 -0700

    Fix button icon for favorite pinning
    * And remove unused icons

--- Editor/UnityBuiltInIcons.cs
+++ Editor/UnityBuiltInIcons.cs
@@ -2,13 +2,9 @@ namespace SelectionHistory.Editor
 {
     public static class UnityBuiltInIcons
     {
-        public const string pickObjectIconName = "d_pick";
         public const string favoriteIconName = "d_Favorite Icon";
-        public const string favoriteEmptyIconName = "d_Favorite";
+        public const string favoriteEmptyIconName = "d_Favorite On Icon";
         public const string removeIconName = "d_ol_minus";
-        public const string tagIconName = "AssetLabelIcon";
         public const string searchIconName = "d_Search Icon";
-        public const string eyeViewToolIconName = "d_ViewToolOrbit";
-        public const string refreshIconName = "TreeEditor.Refresh";
     }
 }
